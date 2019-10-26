e89fcb21aa6348af101d4e970fdabb41991e0d2a
pkey <kutka.paulius@gmail.com>
2019-10-26 14:58:18 +0300

    Filter oneself out

--- src/controllers/node.controller.ts
+++ src/controllers/node.controller.ts
@@ -1,11 +1,14 @@
-import azure from "../client/azure";
 import { ComputeManagementClient } from "@azure/arm-compute";
+import azure from "../client/azure";
+let os = require("os");
 
-const getConnectedDevices = async (resourceGroup) => {
+const getConnectedDevices = async resourceGroup => {
   const computeClient: ComputeManagementClient = await azure.azureClient();
 
-  const connectedDevices = await computeClient.virtualMachines.listAll()
-  return connectedDevices.map(device => device.name);
+  const connectedDevices = await computeClient.virtualMachines.listAll();
+  return connectedDevices
+    .map(device => device.name)
+    .filter(node => node != os.hostname());
 };
 
 export default {
